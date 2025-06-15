4a9a2a45977f46cdc7cb0de2556c71180708b659
Nishkarsh <Nishkarsh.7078@gmail.com>
2025-06-15 20:00:20 +0530

    change in main.py port

--- Backend/main.py
+++ Backend/main.py
@@ -189,3 +189,6 @@ async def predict(file: UploadFile = File(...)):
         "segmentation_mask_base64": mask_b64,
         "overlay_image_base64": overlay_b64,
     }
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
